fe26084a44c36e4baaeed39295de049252de3ea5
Andreas Rozek <A.Rozek@gmx.de>
2021-12-28 14:33:27 +0100

    Create coloredHourNumbers-with-Hands-dark.js

---
+++ ClockFaces/coloredHourNumbers-with-Hands-dark.js
@@ -0,0 +1,118 @@
+  let ScreenWidth  = g.getWidth(),  CenterX = ScreenWidth/2;
+  let ScreenHeight = g.getHeight(), CenterY = ScreenHeight/2;
+
+  let outerRadius = Math.min(CenterX,CenterY) * 0.9 - 10;
+
+  let HourHandLength = outerRadius * 0.5;
+  let HourHandWidth  = 2*3, halfHourHandWidth = HourHandWidth/2;
+
+  let MinuteHandLength = outerRadius * 0.7;
+  let MinuteHandWidth  = 2*2, halfMinuteHandWidth = MinuteHandWidth/2;
+
+  let SecondHandLength = outerRadius * 0.9;
+  let SecondHandOffset = 6;
+
+  let sin = Math.sin, cos = Math.cos;
+
+  let twoPi  = 2*Math.PI;
+  let Pi     = Math.PI;
+  let halfPi = Math.PI/2;
+
+  let HourHandPolygon = [
+    -halfHourHandWidth,halfHourHandWidth,
+    -halfHourHandWidth,halfHourHandWidth-HourHandLength,
+     halfHourHandWidth,halfHourHandWidth-HourHandLength,
+     halfHourHandWidth,halfHourHandWidth,
+  ];
+
+  let MinuteHandPolygon = [
+    -halfMinuteHandWidth,halfMinuteHandWidth,
+    -halfMinuteHandWidth,halfMinuteHandWidth-MinuteHandLength,
+     halfMinuteHandWidth,halfMinuteHandWidth-MinuteHandLength,
+     halfMinuteHandWidth,halfMinuteHandWidth,
+  ];
+
+/**** drawClockFace ****/
+
+  function drawClockFace () {
+    g.setFont('Vector', 22);
+    g.setFontAlign(0,0);
+
+    for (let i = 0; i < 12; i++) {
+      let Phi = i * twoPi/12;
+
+      let x = CenterX + outerRadius * sin(Phi);
+      let y = CenterY - outerRadius * cos(Phi);
+
+      let Color = E.HSBtoRGB(i/12,1,1, true);
+      g.setColor(Color[0]/255,Color[1]/255,Color[2]/255);
+
+      g.drawString(i == 0 ? '12' : '' + i, x,y);
+    }
+  }
+
+/**** transforme polygon ****/
+
+  let transformedPolygon = new Array(HourHandPolygon.length);
+
+  function transformPolygon (originalPolygon, OriginX,OriginY, Phi) {
+    let sPhi = sin(Phi), cPhi = cos(Phi), x,y;
+
+    for (let i = 0, l = originalPolygon.length; i < l; i+=2) {
+      x = originalPolygon[i];
+      y = originalPolygon[i+1];
+
+      transformedPolygon[i]   = OriginX + x*cPhi + y*sPhi;
+      transformedPolygon[i+1] = OriginY + x*sPhi - y*cPhi;
+    }
+  }
+
+/**** draw clock hands ****/
+
+  function drawClockHands () {
+    let now = new Date();
+
+    let Hours   = now.getHours() % 12;
+    let Minutes = now.getMinutes();
+    let Seconds = now.getSeconds();
+
+    let HoursAngle   = (Hours+(Minutes/60))/12 * twoPi - Pi;
+    let MinutesAngle = (Minutes/60)            * twoPi - Pi;
+    let SecondsAngle = (Seconds/60)            * twoPi - Pi;
+
+    g.setColor('#FFFFFF');
+
+    transformPolygon(HourHandPolygon, CenterX,CenterY, HoursAngle);
+    g.fillPoly(transformedPolygon);
+
+    transformPolygon(MinuteHandPolygon, CenterX,CenterY, MinutesAngle);
+    g.fillPoly(transformedPolygon);
+
+    let sPhi = Math.sin(SecondsAngle), cPhi = Math.cos(SecondsAngle);
+
+    g.setColor('#FF0000');
+    g.drawLine(
+      CenterX - SecondHandOffset*sPhi,
+      CenterY + SecondHandOffset*cPhi,
+      CenterX + SecondHandLength*sPhi,
+      CenterY - SecondHandLength*cPhi
+    );
+  }
+
+/**** refreshDisplay ****/
+
+  let Timer;
+  function refreshDisplay () {
+    g.clear();
+
+    g.setColor(0,0,0);
+    g.fillRect(0,0, ScreenWidth,ScreenHeight);
+
+    drawClockFace();
+    drawClockHands();
+
+    let Pause = 1000 - (Date.now() % 1000);
+    Timer = setTimeout(refreshDisplay,Pause);
+  }
+
+  setTimeout(refreshDisplay, 500);                 // enqueue first draw request
