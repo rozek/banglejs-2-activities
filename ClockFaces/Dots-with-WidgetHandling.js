4eddc0bcc719f508fd779402f5d3ed65ead342ee
Andreas Rozek <A.Rozek@gmx.de>
2021-12-28 16:55:09 +0100

    Create Dots-with-WidgetHandling.js

---
+++ ClockFaces/Dots-with-WidgetHandling.js
@@ -0,0 +1,130 @@
+  let ScreenWidth  = g.getWidth(),  CenterX = ScreenWidth/2;
+  let ScreenHeight = g.getHeight(), CenterY = ScreenHeight/2;
+
+  let outerRadius = Math.min(CenterX,CenterY) * 0.9;
+
+  Bangle.loadWidgets();
+
+/**** updateClockFaceSize ****/
+
+  function updateClockFaceSize () {
+    CenterX = ScreenWidth/2;
+    CenterY = ScreenHeight/2;
+
+    outerRadius = Math.min(CenterX,CenterY) * 0.9;
+
+    if (global.WIDGETS == null) { return; }
+
+    let WidgetLayouts = {
+      tl:{ x:0,             y:0,               Direction:0 },
+      tr:{ x:ScreenWidth-1, y:0,               Direction:1 },
+      bl:{ x:0,             y:ScreenHeight-24, Direction:0 },
+      br:{ x:ScreenWidth-1, y:ScreenHeight-24, Direction:1 }
+    };
+
+    for (let Widget of WIDGETS) {
+      let WidgetLayout = WidgetLayouts[Widget.area];     // reference, not copy!
+      if (WidgetLayout == null) { continue; }
+
+      Widget.x = WidgetLayout.x - WidgetLayout.Direction * Widget.width;
+      Widget.y = WidgetLayout.y;
+
+      WidgetLayout.x += Widget.width * (1-2*WidgetLayout.Direction);
+    }
+
+    let x,y, dx,dy;
+    let cx = CenterX, cy = CenterY, r = outerRadius, r2 = r*r;
+
+    x = WidgetLayouts.tl.x; y = WidgetLayouts.tl.y+24; dx = x - cx; dy = y - cy;
+    if (dx*dx + dy*dy < r2) {
+      cy = CenterY + 12; dy = y - cy; r2 = dx*dx + dy*dy; r = Math.sqrt(r2);
+    }
+
+    x = WidgetLayouts.tr.x; y = WidgetLayouts.tr.y+24; dx = x - cx; dy = y - cy;
+    if (dx*dx + dy*dy < r2) {
+      cy = CenterY + 12; dy = y - cy; r2 = dx*dx + dy*dy; r = Math.sqrt(r2);
+    }
+
+    x = WidgetLayouts.bl.x; y = WidgetLayouts.bl.y; dx = x - cx; dy = y - cy;
+    if (dx*dx + dy*dy < r2) {
+      cy = CenterY - 12; dy = y - cy; r2 = dx*dx + dy*dy; r = Math.sqrt(r2);
+    }
+
+    x = WidgetLayouts.br.x; y = WidgetLayouts.br.y; dx = x - cx; dy = y - cy;
+    if (dx*dx + dy*dy < r2) {
+      cy = CenterY - 12; dy = y - cy; r2 = dx*dx + dy*dy; r = Math.sqrt(r2);
+    }
+
+    CenterX = cx; CenterY = cy; outerRadius = r * 0.9;
+  }
+
+  updateClockFaceSize();
+
+/**** custom version of Bangle.drawWidgets (does not clear the widget areas) ****/
+
+  Bangle.drawWidgets = function () {
+    var w = g.getWidth(), h = g.getHeight();
+
+    var pos = {
+      tl:{x:0,   y:0,    r:0, c:0}, // if r==1, we're right->left
+      tr:{x:w-1, y:0,    r:1, c:0},
+      bl:{x:0,   y:h-24, r:0, c:0},
+      br:{x:w-1, y:h-24, r:1, c:0}
+    };
+
+    if (global.WIDGETS) {
+      for (var wd of WIDGETS) {
+        var p = pos[wd.area];
+        if (!p) continue;
+
+        wd.x = p.x - p.r*wd.width;
+        wd.y = p.y;
+
+        p.x += wd.width*(1-2*p.r);
+        p.c++;
+      }
+
+      g.reset();
+
+      if (pos.tl.c || pos.tr.c) {
+        g.setClipRect(0,h-24,w-1,h-1);
+        g.reset();
+      }
+
+      if (pos.bl.c || pos.br.c) {
+        g.setClipRect(0,h-24,w-1,h-1);
+        g.reset();
+      }
+
+      try {
+        for (wd of WIDGETS) {
+          g.clearRect(wd.x,wd.y, wd.x+wd.width-1,23);
+          wd.draw(wd);
+        }
+      } catch (e) { print(e); }
+    }
+  };
+
+  let sin = Math.sin, cos = Math.cos;
+  let twoPi = 2*Math.PI;
+
+  g.clear();
+
+  g.setBgColor(0,0,0);
+  g.clearRect(0,0, ScreenWidth,ScreenHeight);
+
+  g.setBgColor(1,1,1);
+  Bangle.drawWidgets();
+
+  g.setColor(1,1,1);
+  g.setFont('Vector', 22);
+  g.setFontAlign(0,0);
+
+  for (let i = 0; i < 60; i++) {
+    let Phi = i * twoPi/60;
+
+    let x = CenterX + outerRadius * sin(Phi);
+    let y = CenterY - outerRadius * cos(Phi);
+
+    g.fillCircle(x,y, 1);
+  }
