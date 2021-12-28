e67e9d676f8b6d5bb9697458618d75a8c5d9f5d6
Andreas Rozek <A.Rozek@gmx.de>
2021-12-28 11:02:06 +0100

    Create QuarterHourNumbers.js

---
+++ ClockFaces/QuarterHourNumbers.js
@@ -0,0 +1,25 @@
+  let ScreenWidth  = g.getWidth(),  CenterX = ScreenWidth/2;
+  let ScreenHeight = g.getHeight(), CenterY = ScreenHeight/2;
+
+  let outerRadius = Math.min(CenterX,CenterY) * 0.9;
+
+  g.clear();
+
+  g.setColor(0,0,0);
+  g.fillRect(0,0, ScreenWidth,ScreenHeight);
+
+  g.setColor(1,1,1);
+  g.setFont('Vector', 22);
+
+  g.setFontAlign(0,-1);
+  g.drawString('12', CenterX,CenterY-outerRadius);
+
+  g.setFontAlign(1,0);
+  g.drawString('3', CenterX+outerRadius,CenterY);
+
+  g.setFontAlign(0,1);
+  g.drawString('6', CenterX,CenterY+outerRadius);
+
+  g.setFontAlign(-1,0);
+  g.drawString('9', CenterX-outerRadius,CenterY);
+
