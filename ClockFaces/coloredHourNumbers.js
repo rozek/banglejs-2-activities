87245ba912faaf23e613fbc4a284d708a0f35353
Andreas Rozek <A.Rozek@gmx.de>
2021-12-28 11:02:21 +0100

    Create coloredHourNumbers.js

---
+++ ClockFaces/coloredHourNumbers.js
@@ -0,0 +1,27 @@
+  let ScreenWidth  = g.getWidth(),  CenterX = ScreenWidth/2;
+  let ScreenHeight = g.getHeight(), CenterY = ScreenHeight/2;
+
+  let outerRadius = Math.min(CenterX,CenterY) * 0.9 - 10;
+
+  let sin = Math.sin, cos = Math.cos;
+  let twoPi = 2*Math.PI;
+
+  g.clear();
+
+  g.setColor(0,0,0);
+  g.fillRect(0,0, ScreenWidth,ScreenHeight);
+
+  g.setFont('Vector', 22);
+  g.setFontAlign(0,0);
+
+  for (let i = 0; i < 12; i++) {
+    let Phi = i * twoPi/12;
+
+    let x = CenterX + outerRadius * sin(Phi);
+    let y = CenterY - outerRadius * cos(Phi);
+
+    let Color = E.HSBtoRGB(i/12,1,1, true);
+    g.setColor(Color[0]/255,Color[1]/255,Color[2]/255);
+
+    g.drawString(i == 0 ? '12' : '' + i, x,y);
+  }
