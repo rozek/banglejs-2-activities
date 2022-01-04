72b6710078f35eab2d000a305433037b8b751cb1
Andreas Rozek <A.Rozek@gmx.de>
2022-01-04 08:20:02 +0100

    Update Image.js

--- Layouting/Image.js
+++ Layouting/Image.js
@@ -13,27 +13,23 @@
       let Hilite  = Details.hilite || false;
 
       if (Hilite || (Details.bgCol != null)) {
-        let currentBgColor = g.getBgColor();
-          g.setBgColor(Hilite ? g.theme.bgH : Details.bgCol);
-          g.clearRect(x,y, x + Width-1,y + Height-1);
-        g.setBgColor(currentBgColor);
+        g.setBgColor(Hilite ? g.theme.bgH : Details.bgCol);
+        g.clearRect(x,y, x + Width-1,y + Height-1);
       }
 
       if ((Border > 0) && (BorderColor !== null)) {// draw border of layout cell
-        let currentColor = g.getColor();
-          g.setColor(BorderColor || Details.col || g.theme.fg);
+        g.setColor(BorderColor || Details.col || g.theme.fg);
 
-          switch (Border) {
-            case 2:  g.drawRect(x+1,y+1, x+Width-2,y+Height-2);// no break here!
-            case 1:  g.drawRect(x,y,     x+Width-1,y+Height-1); break;
-            default: g.fillPoly([
-              x,y, x+Width,y, x+Width,y+Height, x,y+Height, x,y,
-              x+Border,y+Border, x+Border,y+Height-Border,
-                x+Width-Border,y+Height-Border, x+Width-Border,y+Border,
-                x+Border,y+Border
-            ]);
-          }
-        g.setColor(currentColor);
+        switch (Border) {
+          case 2:  g.drawRect(x+1,y+1, x+Width-2,y+Height-2);// no break here!
+          case 1:  g.drawRect(x,y,     x+Width-1,y+Height-1); break;
+          default: g.fillPoly([
+            x,y, x+Width,y, x+Width,y+Height, x,y+Height, x,y,
+            x+Border,y+Border, x+Border,y+Height-Border,
+              x+Width-Border,y+Height-Border, x+Width-Border,y+Border,
+              x+Border,y+Border
+          ]);
+        }
       }
 
       g.setClipRect(
@@ -48,6 +44,8 @@
         x += Details.ImageWidth/2;
         y += Details.ImageHeight/2;
       }
+
+      g.setColor(Hilite ? g.theme.fgH : Details.col || g.theme.fg);
 
       g.drawImage(Image, x,y, Details.ImageOptions);
     }
