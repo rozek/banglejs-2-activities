ac2f0db9dc461062d9538aba1c1260a4021c0075
Andreas Rozek <A.Rozek@gmx.de>
2022-01-04 14:42:01 +0100

    Create Checkbox.js

---
+++ Layouting/Checkbox.js
@@ -0,0 +1,117 @@
+/**** EventConsumerAtPoint ****/
+
+  let activeLayout;
+
+  function EventConsumerAtPoint (HandlerName, x,y) {
+    let Layout = (activeLayout || {}).l;
+    if (Layout == null) { return; }
+
+    function ConsumerIn (Control) {
+      if (
+        (x < Control.x) || (x >= Control.x + Control.w) ||
+        (y < Control.y) || (y >= Control.y + Control.h)
+      ) { return undefined; }
+
+      if (typeof Control[HandlerName] === 'function') { return Control; }
+
+      if (Control.c != null) {
+        let ControlList = Control.c;
+        for (let i = 0, l = ControlList.length; i < l; i++) {
+          let Consumer = ConsumerIn(ControlList[i]);
+          if (Consumer != null) { return Consumer; }
+        }
+      }
+
+      return undefined;
+    }
+
+    return ConsumerIn(Layout);
+  }
+
+/**** dispatchTouchEvent ****/
+
+  function dispatchTouchEvent (DefaultHandler) {
+    function handleTouchEvent (Button, xy) {
+      if (activeLayout == null) {
+        if (typeof DefaultHandler === 'function') {
+          DefaultHandler();
+        }
+      } else {
+        let Control = EventConsumerAtPoint('onTouch', xy.x,xy.y);
+        if (Control != null) {
+          Control.onTouch(Control, Button, xy);
+        }
+      }
+    }
+    Bangle.on('touch',handleTouchEvent);
+  }
+  dispatchTouchEvent();
+
+/**** dispatchStrokeEvent ****/
+
+  function dispatchStrokeEvent (DefaultHandler) {
+    function handleStrokeEvent (Coordinates) {
+      if (activeLayout == null) {
+        if (typeof DefaultHandler === 'function') {
+          DefaultHandler();
+        }
+      } else {
+        let Control = EventConsumerAtPoint('onStroke', Coordinates.xy[0],Coordinates.xy[1]);
+        if (Control != null) {
+          Control.onStroke(Control, Coordinates);
+        }
+      }
+    }
+    Bangle.on('stroke',handleStrokeEvent);
+  }
+  dispatchStrokeEvent();
+
+  const Checkbox_checked   = require("heatshrink").decompress(atob("ikUgMf/+GgEGoEAlEAgOAgEYsFhw8OjE54OB/EYh4OB+EYj+BwecjFw8OGg0YDocUgECsEAsP//A"));
+  const Checkbox_unchecked = require("heatshrink").decompress(atob("ikUgMf/+GgEGoEAlEAgOAgEYAjkUgECsEAsP//A="));
+
+/**** Checkbox ****/
+
+  function Checkbox (Options) {
+    function renderCheckbox (Details) {
+      let x = Details.x, xAlignment = Details.halign || 0;
+      let y = Details.y, yAlignment = Details.valign || 0;
+
+      let Width  = Details.w, halfWidth  = Width/2  - 10;
+      let Height = Details.h, halfHeight = Height/2 - 10;
+
+      let Padding = Details.pad || 0;
+
+      if (Details.bgCol != null) {
+        g.setBgColor(Details.bgCol);
+        g.clearRect(x,y, x + Width-1,y + Height-1);
+      }
+
+      x += halfWidth  + xAlignment*(halfWidth  - Padding);
+      y += halfHeight + yAlignment*(halfHeight - Padding);
+
+      g.setColor  (Details.col   || g.theme.fg);
+      g.setBgColor(Details.bgCol || g.theme.bg);
+
+      g.drawImage(
+        Details.checked ? Checkbox_checked : Checkbox_unchecked, x,y
+      );
+    }
+
+    function toggleCheckbox (Control) {
+      g.reset();
+
+      Control.checked = ! Control.checked;
+      renderCheckbox(Control);
+    }
+
+    let Result = Object.assign((
+      Options == null ? {} : Object.assign({}, Options.common || {}, Options)
+    ), {
+      type:'custom', render:renderCheckbox, onTouch:toggleCheckbox
+    });
+      let Padding = Result.pad || 0;
+
+      Result.width  = Result.width  || 20 + 2*Padding;
+      Result.height = Result.height || 20 + 2*Padding;
+    return Result;
+  }
