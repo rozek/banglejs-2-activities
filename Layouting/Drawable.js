8dd6e51df91375ba0523e6b7a23a911321a2698d
Andreas Rozek <A.Rozek@gmx.de>
2022-01-04 10:20:05 +0100

    Update Drawable.js

--- Layouting/Drawable.js
+++ Layouting/Drawable.js
@@ -4,26 +4,49 @@
     function renderDrawable (Details) {
       let x = Details.x, xAlignment = Details.halign || 0;
       let y = Details.y, yAlignment = Details.valign || 0;
+
       let Width  = Details.w, DrawableWidth  = Details.DrawableWidth  || Width;
       let Height = Details.h, DrawableHeight = Details.DrawableHeight || Height;
+
       let halfWidth  = Width/2  - DrawableWidth/2;
       let halfHeight = Height/2 - DrawableHeight/2;
-      let Padding = Details.pad || 0;
 
-      if (Details.bgCol != null) {
-        g.setBgColor(Details.bgCol);
+      let Border  = Details.border || 0, BorderColor = Details.BorderColor;
+      let Padding = Details.pad    || 0;
+      let Hilite  = Details.hilite || false;
+
+      if (Hilite || (Details.bgCol != null)) {
+        g.setBgColor(Hilite ? g.theme.bgH : Details.bgCol);
         g.clearRect(x,y, x + Width-1,y + Height-1);
       }
 
-      let DrawableX = x + halfWidth  + xAlignment*(halfWidth  + Padding);
-      let DrawableY = y + halfHeight + yAlignment*(halfHeight + Padding);
+      if ((Border > 0) && (BorderColor !== null)) {// draw border of layout cell
+        g.setColor(BorderColor || Details.col || g.theme.fg);
+
+        switch (Border) {
+          case 2:  g.drawRect(x+1,y+1, x+Width-2,y+Height-2);// no break here!
+          case 1:  g.drawRect(x,y,     x+Width-1,y+Height-1); break;
+          default: g.fillPoly([
+            x,y, x+Width,y, x+Width,y+Height, x,y+Height, x,y,
+            x+Border,y+Border, x+Border,y+Height-Border,
+              x+Width-Border,y+Height-Border, x+Width-Border,y+Border,
+              x+Border,y+Border
+          ]);
+        }
+      }
+
+      let DrawableX = x + halfWidth  + xAlignment*(halfWidth  - Border - Padding);
+      let DrawableY = y + halfHeight + yAlignment*(halfHeight - Border - Padding);
 
       g.setClipRect(
-        Math.max(x+Padding,DrawableX),
-        Math.max(y+Padding,DrawableY),
-        Math.min(x+Width -Padding,DrawableX+DrawableWidth),
-        Math.min(y+Height-Padding,DrawableY+DrawableHeight)
+        Math.max(x+Border+Padding,DrawableX),
+        Math.max(y+Border+Padding,DrawableY),
+        Math.min(x+Width -Border-Padding,DrawableX+DrawableWidth)-1,
+        Math.min(y+Height-Border-Padding,DrawableY+DrawableHeight)-1
       );
+
+      g.setColor  (Hilite ? g.theme.fgH : Details.col   || g.theme.fg);
+      g.setBgColor(Hilite ? g.theme.bgH : Details.bgCol || g.theme.bg);
 
       Callback(DrawableX,DrawableY, DrawableWidth,DrawableHeight, Details);
     }
@@ -33,7 +56,13 @@
     ), {
       type:'custom', render:renderDrawable, cb:Callback
     });
-      Result.width  = (Result.width  || Result.DrawableWidth  || 10) + 2*(Result.pad || 0);
-      Result.height = (Result.height || Result.DrawableHeight || 10) + 2*(Result.pad || 0);
+      let DrawableWidth  = Result.DrawableWidth  || 10;
+      let DrawableHeight = Result.DrawableHeight || 10;
+
+      let Border  = Result.border || 0;
+      let Padding = Result.pad    || 0;
+
+      Result.width  = Result.width  || DrawableWidth  + 2*Border + 2*Padding;
+      Result.height = Result.height || DrawableHeight + 2*Border + 2*Padding;
     return Result;
   }
