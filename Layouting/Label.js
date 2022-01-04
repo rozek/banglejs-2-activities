1eabfd1a225a0accb91f41ff20b8bec8236809f4
Andreas Rozek <A.Rozek@gmx.de>
2022-01-04 08:12:44 +0100

    Update Label.js

--- Layouting/Label.js
+++ Layouting/Label.js
@@ -4,27 +4,47 @@
     function renderLabel (Details) {
       let x = Details.x, xAlignment = Details.halign || 0;
       let y = Details.y, yAlignment = Details.valign || 0;
+
       let Width  = Details.w, halfWidth  = Width/2;
       let Height = Details.h, halfHeight = Height/2;
-      let Padding = Details.pad || 0;
+
+      let Border  = Details.border || 0, BorderColor = Details.BorderColor;
+      let Padding = Details.pad    || 0;
+      let Hilite  = Details.hilite || false;
       let bold    = Details.bold ? 1 : 0;
 
-      g.setColor(Details.col || g.theme.fg || '#000000');
+      if (Hilite || (Details.bgCol != null)) {
+        g.setBgColor(Hilite ? g.theme.bgH : Details.bgCol);
+        g.clearRect(x,y, x + Width-1,y + Height-1);
+      }
+
+      if ((Border > 0) && (BorderColor !== null)) {// draw border of layout cell
+        g.setColor(BorderColor || Details.col || g.theme.fg);
+
+        switch (Border) {
+          case 2:  g.drawRect(x+1,y+1, x+Width-2,y+Height-2);// no break here!
+          case 1:  g.drawRect(x,y,     x+Width-1,y+Height-1); break;
+          default: g.fillPoly([
+            x,y, x+Width,y, x+Width,y+Height, x,y+Height, x,y,
+            x+Border,y+Border, x+Border,y+Height-Border,
+              x+Width-Border,y+Height-Border, x+Width-Border,y+Border,
+              x+Border,y+Border
+          ]);
+        }
+      }
+
+      g.setClipRect(
+        x+Border+Padding,y+Border+Padding,
+        x + Width-Border-Padding-1,y + Height-Border-Padding-1
+      );
+
+      x += halfWidth  + xAlignment*(halfWidth  - Border - Padding);
+      y += halfHeight + yAlignment*(halfHeight - Border - Padding);
+
+      g.setColor(Details.col || g.theme.fg);
 
       if (Details.font != null) { g.setFont(Details.font); }
       g.setFontAlign(xAlignment,yAlignment);
-
-      if (Details.bgCol != null) {
-        g.setBgColor(Details.bgCol);
-        g.clearRect(x,y, x + Width-1 + bold,y + Height-1 + bold);
-      }
-
-      g.setClipRect(x+Padding,y+Padding, x + Width-Padding-1,y + Height-Padding-1);
-
-      x += halfWidth  + xAlignment*(halfWidth +Padding);
-      y += halfHeight + yAlignment*(halfHeight+Padding);
-
-      if (Details.col != null) { g.setColor(Details.col); }
 
       g.drawString(Details.label, x,y);
       if (bold !== 0) {
@@ -39,6 +59,9 @@
     ), {
       type:'custom', render:renderLabel, label:Text || ''
     });
+      let Border  = Result.border || 0;
+      let Padding = Result.pad    || 0;
+
       let TextMetrics;
       if (! Result.width || ! Result.height) {
         if (Result.font == null) {
@@ -52,7 +75,7 @@
       if (Result.col   == null) { Result.col   = g.getColor(); }
       if (Result.bgCol == null) { Result.bgCol = g.getBgColor(); }
 
-      Result.width  = Result.width  || TextMetrics.width  + 2*(Result.pad || 0);
-      Result.height = Result.height || TextMetrics.height + 2*(Result.pad || 0);
+      Result.width  = Result.width  || TextMetrics.width  + 2*Border + 2*Padding;
+      Result.height = Result.height || TextMetrics.height + 2*Border + 2*Padding;
     return Result;
   }
