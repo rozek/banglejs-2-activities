87be70c7669b01dcdeba8d8bfc44b4992cb3cba6
Andreas Rozek <A.Rozek@gmx.de>
2022-01-05 10:45:37 +0100

    Update ButtonDemo.js

--- Layouting/ButtonDemo.js
+++ Layouting/ButtonDemo.js
@@ -112,7 +112,11 @@
 
       let TextMetrics;
       if (! Result.width || ! Result.height) {
-        if (Options.font != null) { g.setFont(Options.font); }
+        if (Result.font == null) {
+          Result.font = g.getFont();
+        } else {
+          g.setFont(Result.font);
+        }
         TextMetrics = g.stringMetrics(Result.label);
       }
 
